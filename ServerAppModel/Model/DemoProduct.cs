26f6250e08eb2b397d764b748fea425f2a71c004
SabitBinHamidBJIT <sabit.hamid@bjitgroup.com>
2026-02-03 17:03:42 +0600

    second commit1

---
+++ ServerAppModel/Model/DemoProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerAppModel.Model
+{
+    public  class DemoProduct
+    {
+        public int Id {  get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Price { get; set; }
+        public List<DemoProductProperty> Properties { get; set; }
+    }
+}
---
+++ ServerAppModel/Model/DemoProductProperty.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServerAppModel.Model
+{
+    public class DemoProductProperty
+    {
+        public int Id
+            { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
